e9a77ad45ab11cb8ff1af2a83b77a4f0553c0138
Tatsuru Hisanaga <tatsu72x@gmail.com>
2024-04-18 20:55:30 +0900

    ✨ [add] Proceed POST request from Webhook

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 export const main = () => {
   console.log('🐛 debug : テスト')
 }
+
+
+/**
+ * WebhookからのPOSTリクエストを処理する
+ * @param e
+ */
+export const doPost = (e: any) => {
+  const EVENTS = JSON.parse(e.postData.contents).events
+  for (const event of EVENTS) {
+    execute(event)
+  }
+}
